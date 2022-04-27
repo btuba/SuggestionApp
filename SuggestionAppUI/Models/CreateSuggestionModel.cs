d0f1a0e829f928a32826f242b1a19e10db36d5f9
buse t kocabıyık <btubakocabiyik@gmail.com>
2022-04-28 01:24:43 +0300

    Add Create Page

---
+++ SuggestionAppUI/Models/CreateSuggestionModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuggestionAppUI.Models
+{
+    public class CreateSuggestionModel
+    {
+        [Required]
+        [MaxLength(length:75)]
+        public string Suggestion { get; set; }
+        
+        [Required]
+        [MinLength(length:1)]
+        [Display(Name ="Category")]
+        public string CategoryId { get; set; }
+
+        [MaxLength(length:500)]
+        public string Description { get; set; }
+    }
+}
